8d252b923b379c84eb4a48f59468df26c86710c3
Francois Dorval <fdorval@hotmail.com>
2021-09-05 13:49:56 -0400

    add gitbash cp2media.sh to cp executable outside git repos into parent media

---
+++ cp2media.sh
@@ -0,0 +1,5 @@
+# bash cp2media.sh
+cp Normal/launcher/launcher.exe ../media/Grit.exe
+cp Normal/engine/engine.exe ../media/Grit.dat
+cp Normal/extract/extract.exe ../media/Tools
+cp Normal/GritXMLConverter/GritXMLConverter.exe ../media/Tools
