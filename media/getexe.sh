9b48b32283a97e5518eab01e9335a451edbc5c59
Francois Dorval <fdorval@hotmail.com>
2021-09-10 19:05:53 -0400

    add a script to copy normal build exe into subdir media

---
+++ media/getexe.sh
@@ -0,0 +1,5 @@
+# bash getexe.sh
+cp ../Normal/launcher/launcher.exe ./Grit.exe
+cp ../Normal/engine/engine.exe ./Grit.dat
+cp ../Normal/extract/extract.exe ./Tools
+cp ../Normal/GritXMLConverter/GritXMLConverter.exe ./Tools
